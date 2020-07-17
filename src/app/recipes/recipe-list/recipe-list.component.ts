1b0e975f3e26c6af5b0940236faef681a59443fa
Laura <laura.vespa96@gmail.com>
2020-07-17 15:25:27 +0200

    init

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelectet = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Just a test',
+      'https://live.staticflickr.com/4168/34641495421_038cd633ec_b.jpg'),
+
+    new Recipe('A Test by Laura', 'Just a Laura test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRVCjFJAXYIL7b0YslFA5ALoHUfhG3ZVtKJ1A&usqp=CAU')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelectet.emit(recipe);
+  }
+
+}
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    //name: string;
+    //amount: number;
+
+    //constructor(name: string; amount: number){
+//
+  //  } Everything here can be done by the following shortcut
+
+  constructor(public name: string, public amount: number){
+
+  }
+}
